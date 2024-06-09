20f551b1a7f2b2740061c3f914aa3cace7a3a629
hendrahartanto <hendrazhuo10@gmail.com>
2024-06-09 19:50:05 +0700

    udpate

---
+++ AOL_MachineLearning/src/components/navbar/Navbar.tsx
@@ -0,0 +1,15 @@
+import "./navbar.css";
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <div className="page-center">
+        <div className="left-side">
+          <p className="icon">OpenDoor</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ AOL_MachineLearning/src/pages/home/HomePage.tsx
@@ -0,0 +1,12 @@
+import Navbar from "../../components/navbar/Navbar";
+import "./homePage.css";
+
+const HomePage = () => {
+  return (
+    <div className="home-page">
+      <Navbar />
+    </div>
+  );
+};
+
+export default HomePage;
